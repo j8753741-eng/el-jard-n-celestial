0cb37f6da901b8796763ebda8f7604c859d3eef4
j8753741-eng <j8753741@gmail.com>
2026-06-11 22:23:28 -0500

    Add event listener to open music link

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("musicButton").addEventListener("click", () => {
 
+    window.open(
+        "https://youtu.be/HpphFd_mzXE?si=YyvZsj3UhQLpjqgi",
+        "_blank"
+    );
+
+}); 
