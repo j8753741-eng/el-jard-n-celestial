02f87de3e6389bbbf3a1a54ec3b5abe36b336eaa
j8753741-eng <j8753741@gmail.com>
2026-06-11 22:40:27 -0500

    Add modal open/close functionality
    Implement modal open and close functionality with event listeners.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// El Jardín Celestial comenzará a cobrar vida aquí.
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+const overlay = document.getElementById("overlay");
+const modal = document.getElementById("modal");
+
+openModal.addEventListener("click", () => {
+
+    overlay.classList.add("show");
+    modal.classList.add("show");
+
+});
+
+closeModal.addEventListener("click", () => {
+
+    overlay.classList.remove("show");
+    modal.classList.remove("show");
+
+});
+
+overlay.addEventListener("click", () => {
+
+    overlay.classList.remove("show");
+    modal.classList.remove("show");
+
+});
